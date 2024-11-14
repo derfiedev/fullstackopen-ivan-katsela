47ff2ff86580fc00ef38f6c00945367ae3d4fcc1
derfie <dikessdikes@gmail.com>
2024-11-14 07:47:48 +0100

    1.8

--- exercisesp1c/src/App.jsx
+++ exercisesp1c/src/App.jsx
@@ -5,20 +5,63 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [score, setScore] = useState(0)
+  const [percPositive, setPercPositive] = useState(0)
+  
+
+  const Statistics  = (props) => {
+    return(
+      <div>
+        <h1>statistics</h1>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {total}</p>
+        <p>Average: {score}</p>
+        <p>Positive: {percPositive}</p>
+      </div>
+    )
+  }
+  
+
+  const updateStats = () =>{
+    setTotal(good + neutral + bad + 1)
+    setScore((good * 1 + bad * (-1))/(good + bad))
+    setPercPositive(good/(good+neutral+bad)*100)
+  }
+
+  const handleGoodClick = () =>{
+    setGood(good + 1)
+    updateStats()
+  }
+  const handleNeutralClick = () =>{
+    setNeutral(neutral + 1)
+    updateStats()
+  }
+  const handleBadClick = () =>{
+    setBad(bad + 1)
+    updateStats()
+  }
+
+  
 
   return (
     <div>
       <h1>give feedback</h1>
       <div>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <button onClick={handleGoodClick}>good</button>
+        <button onClick={handleNeutralClick}>neutral</button>
+        <button onClick={handleBadClick}>bad</button>
       </div>
-      <h1>statistics</h1>
+      <Statistics/>
+      {/* <h1>statistics</h1>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
-
+      <p>Total: {total}</p>
+      <p>Average: {score}</p>
+      <p>Positive: {percPositive}</p> */}
     </div>
   )
 }
